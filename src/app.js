6c58f54af3cde9467020668de8d82b15461b7183
MarcosManzino <marcosmanzino01@gmail.com>
2023-05-14 19:40:21 -0300

    primer entrega

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import productRouter from "./router/products.router.js";
+import cartRouter from "./router/carts.router.js";
+
+const server = express();
+
+server.listen(8080, ()=>{console.log("Server UP!")})
+
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+
+server.use("/api/products", productRouter)
+server.use("/api/carts", cartRouter)
